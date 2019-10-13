e05114e441cbd0e36f219ab9916e955d96d9c574
Albert ChenX <chenxeed@gmail.com>
2019-10-13 16:02:39 +0800

    fix(video): missing key on loop VideoItem - some video data returned is a channel, so it has no id.videoId - map the videos to only allow video with videoId

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,15 +4,18 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos, onVideoSelect }) => {
   // has props.videos
 
-  const renderedList = videos.map(video => {
-    return (
-      <VideoItem
-        key={video.id.videoId}
-        video={video}
-        onVideoSelect={onVideoSelect}
-      />
-    );
-  });
+  const renderedList = videos
+    // some video data are a "channel", so it can't be rendered as video
+    .filter(video => video.id && video.id.videoId)
+    .map(video => {
+      return (
+        <VideoItem
+          key={video.id.videoId}
+          video={video}
+          onVideoSelect={onVideoSelect}
+        />
+      );
+    });
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 export default VideoList;
