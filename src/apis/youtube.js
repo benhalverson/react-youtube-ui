e7a31fe0e450f7c9f199f3a52bf3b66347015eb5
Ben Halverson <benhalverson33@gmail.com>
2021-10-29 20:29:23 -0700

    fix: 🐛 updated api key to be in an env var

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyBAbTF3CJTzzVZGqYkWdTP-la4mmn5xBxc';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 //KEY is restricted in google dev console.
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
